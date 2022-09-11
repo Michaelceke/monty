9dda0108a07be67a6f153c540373a9acaf3c94f3
Michaelceke <ekemicheal@gmail.com>
2022-09-11 16:29:59 -0700

    fixed

--- monty.h
+++ monty.h
@@ -20,8 +20,9 @@
  */
 typedef struct var_s
 {
-    int len_queue;
-    size_t len_stack;
+	int len_queue;
+
+	size_t len_stack;
 } var_t;
 
 /* global flag contains queue and stack length */
@@ -38,22 +39,25 @@ extern var_t var;
  */
 typedef struct stack_s
 {
-    int n;
-    struct stack_s *prev;
-    struct stack_s *next;
+	int n;
+
+	struct stack_s *prev;
+
+	struct stack_s *next;
 } stack_t;
+
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
- *
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
-    char *opcode;
-    void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode
+
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 stack_t *add_node(stack_t **stack, const int n);
